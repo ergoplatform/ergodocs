3ef801358ef61ad5c649dee7ac7a6acc52393d41
Mark Glasgow <markglasgow@gmail.com>
2021-11-16 22:07:11 +0000

    sexy bg image

---
+++ docs/assets/extra.js
@@ -0,0 +1,4 @@
+document.body.style.backgroundImage = "url('/assets/frame_5.png')";
+document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.backgroundSize = "auto";
+document.body.style.backgroundPosition = "center right";
