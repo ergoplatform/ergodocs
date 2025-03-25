9cc9db20cd521c79156459a83412e684171278e5
M Glasgow <markglasgow@gmail.com>
2025-03-25 12:26:02 +0000

    deploy.sh

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Start deploy"
-ssh -tq root@213.239.193.208 '/bin/bash -l -c "source ~/.nvm/nvm.sh; cd /var/www/ergodocs/tools; git pull; rm -rf /site;  mkdocs build; cd /data/explorer;  docker-compose restart nginx"'
+ssh -tq root@213.239.193.208 '/bin/bash -l -c "source ~/.nvm/nvm.sh; cd /var/www/ergodocs/tools; git pull; rm -rf /site;  mkdocs build;"'
 echo "Deployed Successfully!"
 
 exit 0
